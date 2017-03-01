5c43c5a956386c97bf3a826c6b1f72d09d621dab
Artem <ak973@yandex.ru>
2017-03-01 19:50:01 +0400

    Добавлены стартовые файлы проекта.

---
+++ src/main/java/servlets/MainServlet.java
@@ -0,0 +1,22 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet(name = "MainServlet", urlPatterns = {"/"})
+public class MainServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("test");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("name", "Emperor protect.");
+
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Devcolibri.com</title>
+</head>
+<body>
+<h1>For Emperor and Imperium! ${name} ${name1}</h1>
+<form method="POST">
+    <p><input value="Введите текст"></p>
+    <p><input type="submit" value="Отправить">
+        <input type="reset" value="Очистить"></p>
+</form>
+</body>
+</html>
