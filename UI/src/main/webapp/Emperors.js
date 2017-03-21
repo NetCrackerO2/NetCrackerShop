49e818d3774ae05a30a2f83fd46a6d6cf446340a
Aidar Fattakhov <asschool@yandex.ru>
2017-03-21 22:53:28 +0400

    Revert "first version"
    This reverts commit b1b816a1348b3b35123adf7b720dbc4425c5eb12.

--- UI/src/main/webapp/Emperors.js
+++ UI/src/main/webapp/Emperors.js
@@ -1,91 +1,59 @@
-function Page(url, title, request, callback){
-	this.urlRegExp = new RegExp("^"+url+"$");
-	this.title = title;
-	this.request = request;
-	this.callback = callback;
-};
-Page.prototype.suitable = function(url){
-	return this.urlRegExp.test(url);
-};
-Page.prototype.load = function(data, url){
-	console.log("loaded " + url);
-	var title = "";
-	if (typeof this.title === 'function') {
-		title = this.title(this, data);
-	}else{
-		title = this.title;
-	}
-	document.title = title;
-	window.history.pushState(data, title, url);
-	$("#items").empty();
-	this.callback(this, data);
-};
-Page.prototype.go = function(url){
-	console.log("go to " + url);
-	if(this.request) {
-		var request = this.request;
-		var matches = this.urlRegExp.exec(url);
-		if(matches.length > 1){
-			request += matches[1];
-		}
-		if(matches.length > 2){
-			request += "/" + matches[2];
-		}
-		if(matches.length > 3){
-			request += "/" + matches[3];
-		}
-		var self = this;
-		$.getJSON(request, function (data) {
-			self.load(data, url);
-		});
-	}
-	else
-		this.load(null, url);
-};
-var pager = {
-	pages: [],
-	add: function(page){
-		this.pages.push(page);
-	},
-	getPageByUrl: function(url){
-		for(var i in this.pages)
-			if(this.pages[i].suitable(url))
-				return this.pages[i];
-		return this.pages[0];
-	},
-	go: function(url){
-		this.getPageByUrl(url).go(url);
-	}
-};
-window.onpopstate = function(e){
-	pager.getPageByUrl(document.location).load(e.state);
-};
-pager.add(new Page(
-	"/404",
-	"Not found",
-	undefined,
-	function(page, data){
-		$("#items").text("Error: not found");
-	}
-));
-pager.add(new Page(
-	".*//[^/]+/",
-	"Categories",
-	"/Shop/rest/category/",
-	function(page, data){
-		for(var i in data){
-			$("#items").append('<article class="z1"><h2>'+data[i].name+'</h2></article>');
-		}
-	}
-));
-pager.add(new Page(
-	".*//[^/]+/product/([0-9]*)",
-	function(page, data){
-		return "Product "+data.name;
-	},
-	"/Shop/rest/product/",
-	function(page, data){
-	}
-));
+function start(categoryId) {
+    $.getJSON('http://localhost:8080/Shop/rest/product/getbycategory/' + categoryId, function (data) {
+        var items = document.getElementById("items");
 
-pager.go(document.location);
+        $.each(data, function (key, value) {
+            var newContent;
+
+            var newArticle = document.createElement("article");
+            newArticle.className = "z1";
+
+            var newH2 = document.createElement("h2");
+            newContent = document.createTextNode(value.name);
+            newH2.appendChild(newContent);
+
+            var newDesc = document.createElement("desc");
+            newContent = document.createTextNode(value.description);
+            newDesc.appendChild(newContent);
+
+            var newPrice = document.createElement("price");
+            newContent = document.createTextNode(value.price);
+            newPrice.appendChild(newContent);
+
+            var newLink = document.createElement("a");
+            newLink.href = "http://localhost:8080/Shop/rest/order/addproductinorder/1?productId=" + value.id
+                + "&count=13&price=13.13";
+            newLink.textContent = "Добавить в корзину";
+
+            newArticle.appendChild(newH2);
+            newArticle.appendChild(newDesc);
+            newArticle.appendChild(newPrice);
+            newArticle.appendChild(newLink);
+
+
+            items.appendChild(newArticle);
+        });
+    });
+}
+
+function fillCategories() {
+    $.getJSON('http://localhost:8080/Shop/rest/category/', function (data) {
+        var categoriesSelect = document.categoriesForm.categories;
+
+        $.each(data, function (key, value) {
+            categoriesSelect.options[categoriesSelect.options.length] = new Option(value.name, value.id);
+        })
+    });
+}
+
+function fillProducts() {
+    var elements = document.getElementsByClassName("z1");
+    while (elements.length > 0) {
+        elements[0].remove();
+    }
+
+    var categoriesSelect = document.categoriesForm.categories;
+    var selectedIndex = categoriesSelect.selectedIndex;
+    var value = categoriesSelect.options[selectedIndex].value;
+    start(value);
+}
